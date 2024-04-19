b6dcbdce46860299dad78511e47641d866b858d8
Harshit <harshitjaintata@gmail.com>
2024-04-19 23:53:00 +0530

    Created all the pages and routes for the same!

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Logo from '../images/mern-blog-assets-main/logo-color.png';
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
+
+const Header = () => {
+  return (
+   <nav>
+      <div className="container nav_container">
+        <Link to='/' className='nav_logo'>
+          <img src={Logo} alt="Navbar Logo" />
+        </Link>
+        <ul className='nav__menu'>
+          <li><Link to="/profile">Harshi Jain</Link></li>
+          <li><Link to="/create">Create Post</Link></li>
+          <li><Link to="/authors">Authors</Link></li>
+          <li><Link to="/logout">Logout</Link></li>
+        </ul>
+        <button className='nav__toggle-btn'>
+          <AiOutlineClose/>
+        </button>
+
+      </div>
+   </nav>
+  )
+}
+
+export default Header
